3746b29ea5b1aafdb98a6dd8abd7add0b6d4f0e6
Samuel Just <sam.just@inktank.com>
2014-04-10 17:38:22 -0700

    ReplicatedPG::cancel_flush: requeue dup_ops even if !op
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -6236,8 +6236,9 @@ void ReplicatedPG::cancel_flush(FlushOpRef fop, bool requeue)
     Mutex::Locker l(osd->objecter_lock);
     osd->objecter->op_cancel(fop->objecter_tid, -ECANCELED);
   }
-  if (fop->ctx->op && requeue) {
-    requeue_op(fop->ctx->op);
+  if (requeue) {
+    if (fop->ctx->op)
+      requeue_op(fop->ctx->op);
     requeue_ops(fop->dup_ops);
   }
   if (fop->blocking) {
